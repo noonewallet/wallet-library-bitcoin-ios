aa128b1d580bc21601eeaa03b6a05ba6bea0936c
Aleksei Muraveinik <aleksei.muraveinik@gmail.com>
2024-10-28 15:48:47 +0100

    NN-790 Change local submodules for remote

--- Pods/Target Support Files/Pods-WalletLibBitcoin/Pods-WalletLibBitcoin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WalletLibBitcoin : NSObject
-@end
-@implementation PodsDummy_Pods_WalletLibBitcoin
-@end
--- Pods/Target Support Files/WalletLibCrypto/WalletLibCrypto-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_WalletLibCrypto : NSObject
-@end
-@implementation PodsDummy_WalletLibCrypto
-@end
